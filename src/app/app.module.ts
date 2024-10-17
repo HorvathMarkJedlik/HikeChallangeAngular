c8bba6d4f506aee66ed65d9ae126695d76312601
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-10-17 09:37:21 +0200

    model letrehozas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HikeListComponent } from './hike-list/hike-list.component';
+import { HikeItemComponent } from './hike-item/hike-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HikeListComponent,
+    HikeItemComponent
   ],
   imports: [
     BrowserModule,
